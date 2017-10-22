4db93f9b7d085d4bb2ec6b36b63dcedf36a29bb0
Steve Smith <stevesmith2609@gmail.com>
2017-10-22 15:37:02 +0100

    Add User to Santa action

--- app/models/santa.rb
+++ app/models/santa.rb
@@ -3,4 +3,22 @@ class Santa < ApplicationRecord
   has_many :users, through: :participants
   has_many :pairings
   has_many :rules
+
+  def add_user(user)
+    users << user
+
+    users.each do |u|
+      next if user == u
+
+      pairings.create(giver: u, receiver: user, status: 'open')
+      pairings.create(giver: user, receiver: u, status: 'open')
+
+      # Will need to re-enforce all rules!
+      # enforce_all_rules
+    end
+  end
+
+  def enforce_all_rules
+    rules.order!(updated_at: :desc).each(&:enforce)
+  end
 end
